c7a2031ffe36cee7159164db07e4ca01214fdf16
Uche Emmanuel <urtchman04@yahoo.co.uk>
2025-04-17 20:42:32 -0400

    Create budpayConfig.js

---
+++ src/config/budpayConfig.js
@@ -0,0 +1,5 @@
+const budpayConfig = {
+    publicKey: 'pk_test_mxavdwx9zkulbvwdsuf7jixdvgxkeddohwspll',
+    secretKey: 'sk_test_9u1aflos0ce2nkhvb5kdwkijp3ukgf6ggousfik'
+};
+export default budpayConfig;
