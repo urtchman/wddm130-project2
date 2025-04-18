12ba7eea733d6d2c0f94b67ee28636529f882d6f
Uche Emmanuel <urtchman04@yahoo.co.uk>
2025-04-17 20:43:33 -0400

    Delete config directory

--- config/dbConfig.js
+++
@@ -1,9 +0,0 @@
-const dbConfig = { 
-    database: 'nairaswapdb', 
-    user: 'neondb_owner', 
-    password: 'npg_S7FWGca5gkDi',  
-    host: 'ep-frosty-truth-a5cza0i5-pooler.us-east-2.aws.neon.tech',
-    dbport: 5432
-};
-
-export default dbConfig;
--- config/swapConfig.js
+++
@@ -1,5 +0,0 @@
-const swapConfig = { 
-    key: "18a37c18dc9de81761f20278b4ddef6f",
-    baseUrl: "https://api.exchangerate.host/"
-}
-export default swapConfig;
